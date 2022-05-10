6713af71dc68746ef3770292e85f89e409579c57
aridguy <rotimiariyo@gmail.com>
2022-05-10 01:10:51 +0100

    still setting up the main page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Home = () => {
     return(
         <>
-        <div class="animated wow bounceInLeft container-fluid p-5 bg-primary text-white text-center">
-  <h1>My First Bootstrap Page</h1>
+        <div className="container-fluid p-5 bg-primary text-white text-center">
+  <h1 className="animated bounceIn">My First Bootstrap Page</h1>
   <p>Resize this responsive page to see the effect!</p> 
 </div>
         </>
